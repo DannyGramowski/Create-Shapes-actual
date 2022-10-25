0faf649bbbb506a9d2cc35a89b49362a4c617e93
DannyGramowski <dgramowsk@gmail.com>
2022-10-24 23:16:20 -0400

    added mesh surface area and volume calculations. also removed extra test scenes.

--- Assets/Scripts/MathUtil.cs
+++ Assets/Scripts/MathUtil.cs
@@ -1,11 +1,46 @@
-﻿using Codice.Client.BaseCommands.BranchExplorer.ExplorerData;
-using UnityEngine;
+﻿using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 namespace Create_Shape {
     public static class MathUtil {
 
-        public static float Volume(Mesh mesh) {
-            return -1;
+        //I also trust this https://stackoverflow.com/questions/1406029/how-to-calculate-the-volume-of-a-3d-mesh-object-the-surface-of-which-is-made-up
+        public static float MeshVolume(Mesh mesh) {
+            float output = 0;
+            for (int i = 0; i < mesh.triangles.Length; i += 3) {
+                Vector3 a = mesh.vertices[mesh.triangles[i]];
+                Vector3 b = mesh.vertices[mesh.triangles[i+1]];
+                Vector3 c = mesh.vertices[mesh.triangles[i+2]];
+                output += SignedVolumeOfTriangle(a, b, c);
+            }
+
+            return Mathf.Abs(output);        }
+        
+        private static float SignedVolumeOfTriangle(Vector3 p1, Vector3 p2, Vector3 p3) {
+            var v321 = p3.x*p2.y*p1.z;
+            var v231 = p2.x*p3.y*p1.z;
+            var v312 = p3.x*p1.y*p2.z;
+            var v132 = p1.x*p3.y*p2.z;
+            var v213 = p2.x*p1.y*p3.z;
+            var v123 = p1.x*p2.y*p3.z;
+            return (1.0f/6.0f)*(-v321 + v231 + v312 - v132 - v213 + v123);
+        }
+
+        public static float MeshSurfaceArea(Mesh mesh) {
+            float output = 0;
+            for (int i = 0; i < mesh.triangles.Length; i += 3) {
+                Vector3 a = mesh.vertices[mesh.triangles[i]];
+                Vector3 b = mesh.vertices[mesh.triangles[i+1]];
+                Vector3 c = mesh.vertices[mesh.triangles[i+2]];
+                output += TriangleArea(a, b, c);
+            }
+
+            return output;
+        }
+
+        //I trust this https://math.stackexchange.com/questions/128991/how-to-calculate-the-area-of-a-3d-triangle
+        public static float TriangleArea(Vector3 a, Vector3 b, Vector3 c) {
+            return Vector3.Cross(b-a, c-a).magnitude / 2f;
         }
 
         public static double Integrate(float lower, float upper, Equation eq) {
--- Assets/Tests/Runtime/Math Tests.cs
+++ Assets/Tests/Runtime/Math Tests.cs
@@ -6,11 +6,70 @@ using Create_Shape;
 
 
 public class MathTests {
+    private Mesh cube;
+    private const int CubeWidth = 2;
+    
+    [SetUp]
+    public void SetUp() {
+        #region createCube
+
+        Vector3[] vertices = {
+            new Vector3 (0, 0, 0),
+            new Vector3 (CubeWidth, 0, 0),
+            new Vector3 (CubeWidth, CubeWidth, 0),
+            new Vector3 (0, CubeWidth, 0),
+            new Vector3 (0, CubeWidth, CubeWidth),
+            new Vector3 (CubeWidth, CubeWidth, CubeWidth),
+            new Vector3 (CubeWidth, 0, CubeWidth),
+            new Vector3 (0, 0, CubeWidth),
+        };
+        
+        int[] triangles = {
+            0, 2, 1, //face front
+            0, 3, 2,
+            2, 3, 4, //face top
+            2, 4, 5,
+            1, 2, 5, //face right
+            1, 5, 6,
+            0, 7, 4, //face left
+            0, 4, 3,
+            5, 4, 7, //face back
+            5, 7, 6,
+            0, 6, 7, //face bottom
+            0, 1, 6
+        };
+
+        cube = new Mesh();
+        cube.vertices = vertices;
+        cube.triangles = triangles;
+        #endregion
+    }
+    
     [Test]
     public void TestIntegral() {
         var num = MathUtil.Integrate(0, 1, new Equation("x"));
         Assert.AreEqual(0.5, num, 0.01f);
     }
-    
+
+    [Test]
+    public void TestTriangleArea() {
+        var a = new Vector3(0, 0, 0);
+        var b = new Vector3(2, 0, 0);
+        var c = new Vector3(0, 1, 0);
+        var area = MathUtil.TriangleArea(a,b,c);
+        Assert.AreEqual(1, area);
+    }
+
+    [Test]
+    public void TeshMeshVolume() {
+        var volume = MathUtil.MeshVolume(cube);
+        Assert.AreEqual(8, volume, 0.01);
+    }
+
+    [Test]
+    public void TestMeshSurfaceArea() {
+        var sa = MathUtil.MeshSurfaceArea(cube);
+        Assert.AreEqual(24, sa, 0.01);
+    }
     
 }
